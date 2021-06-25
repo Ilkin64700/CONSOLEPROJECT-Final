340fd0144c06d04249b8284de367cc5bfc3bc924
ilkin64700 <ilkinsz@code.edu.az>
2021-06-25 12:49:51 +0400

    Imterface ve Method yaradildi. Methoddaki setler teyin edildi

--- ConsoleProject/Interfaces/IHumanResourceManager.cs
+++ ConsoleProject/Interfaces/IHumanResourceManager.cs
@@ -9,7 +9,14 @@ namespace ConsoleProject.Interfaces
 {
     interface IHumanResourceManager
     {
-       
+        List<Department> Departments { get; set; }
+        void AddDepartment(string name, int workerlimit, double salarylimit);
+        List<Department> GetDepartments();
+        void EditDepartment(string name, string newname);
+        void AddEmployee(string fullname, string position, double salary, string departmentname, string Id, List<Employee> Employees);
+        void RemoveEmployee(string Id, string departmmentname, Employee employee, List<Employee> Employees);
+        void EditEmployee(string Id, string fullname, double salary, string position, List<Employee> Employees);
+
 
 
     }
